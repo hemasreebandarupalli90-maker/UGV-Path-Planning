599eac04f1fea27fc46a057e3f3c5a4f6b809b74
hemasreebandarupalli90-maker <hemasreebandarupalli90@gmail.com>
2026-03-19 19:08:24 +0530

    Refactor pathfinding code and variable names

--- 06_Advanced_Pathfinding/ugv_static.py
+++ 06_Advanced_Pathfinding/ugv_static.py
@@ -1,84 +1,63 @@
 import heapq
 import random
 
-SIZE = 20  # (use 70 for full scale)
-OBSTACLE_DENSITY = 0.2  # change: 0.1, 0.2, 0.3
-
+SIZE = 15
+DENSITY = 0.2
 
 def generate_grid():
     grid = [[0 for _ in range(SIZE)] for _ in range(SIZE)]
-
     for i in range(SIZE):
         for j in range(SIZE):
-            if random.random() < OBSTACLE_DENSITY:
-                grid[i][j] = 1  # obstacle
-
+            if random.random() < DENSITY:
+                grid[i][j] = 1
     return grid
 
 
-def get_neighbors(x, y):
-    moves = [(1,0), (-1,0), (0,1), (0,-1)]
-    return [(x+dx, y+dy) for dx, dy in moves]
+def neighbors(x, y):
+    return [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
 
 
-def dijkstra_grid(grid, start, goal):
+def dijkstra(grid, start, goal):
     pq = [(0, start)]
-    visited = set()
     parent = {}
+    visited = set()
 
     while pq:
-        cost, (x, y) = heapq.heappop(pq)
+        cost, node = heapq.heappop(pq)
 
-        if (x, y) in visited:
+        if node in visited:
             continue
 
-        visited.add((x, y))
+        visited.add(node)
 
-        if (x, y) == goal:
+        if node == goal:
             break
 
-        for nx, ny in get_neighbors(x, y):
+        for nx, ny in neighbors(*node):
             if 0 <= nx < SIZE and 0 <= ny < SIZE:
                 if grid[nx][ny] == 0:
-                    heapq.heappush(pq, (cost + 1, (nx, ny)))
-                    parent[(nx, ny)] = (x, y)
+                    heapq.heappush(pq, (cost+1, (nx,ny)))
+                    parent[(nx,ny)] = node
 
-    # reconstruct path
     path = []
     node = goal
-
     while node in parent:
         path.append(node)
         node = parent[node]
 
     path.append(start)
     path.reverse()
-
     return path
-
-
-def print_grid(grid, path):
-    for i in range(SIZE):
-        for j in range(SIZE):
-            if (i, j) in path:
-                print("P", end=" ")
-            elif grid[i][j] == 1:
-                print("X", end=" ")
-            else:
-                print(".", end=" ")
-        print()
 
 
 if __name__ == "__main__":
     grid = generate_grid()
-
-    start = (0, 0)
+    start = (0,0)
     goal = (SIZE-1, SIZE-1)
 
     grid[0][0] = 0
     grid[goal[0]][goal[1]] = 0
 
-    path = dijkstra_grid(grid, start, goal)
+    path = dijkstra(grid, start, goal)
 
     print("Path Length:", len(path))
-    print_grid(grid, path)
